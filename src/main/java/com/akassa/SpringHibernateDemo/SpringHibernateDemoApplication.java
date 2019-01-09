55e1a343d5443d0705ffe3bf20c140720c7590a3
akassa1990 <ahmed.kassa1990@gmail.com>
2019-01-08 21:46:11 -0500

    Restoring Project Files

--- src/main/java/com/akassa/SpringHibernateDemo/SpringHibernateDemoApplication.java
+++ src/main/java/com/akassa/SpringHibernateDemo/SpringHibernateDemoApplication.java
@@ -1,12 +1,12 @@
-package com.codingexercise.state;
+package com.akassa.SpringHibernateDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StateMicroserviceApplication {
+public class SpringHibernateDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StateMicroserviceApplication.class, args);
+		SpringApplication.run(SpringHibernateDemoApplication.class, args);
 	}
 }

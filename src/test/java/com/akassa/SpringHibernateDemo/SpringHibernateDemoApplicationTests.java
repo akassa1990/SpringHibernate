a9fc4bcdc2e856f5181c153f8963f03059fff3ff
akassa1990 <ahmed.kassa1990@gmail.com>
2019-01-08 19:07:30 -0500

    initial commit

---
+++ src/test/java/com/akassa/SpringHibernateDemo/SpringHibernateDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.akassa.SpringHibernateDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHibernateDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
